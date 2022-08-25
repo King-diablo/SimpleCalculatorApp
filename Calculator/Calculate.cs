fbdbe627fb52e399b94cc193a004ba4cd073b7ec
Chikeluba Obiora-Eze <chikelubaobioraeze@gmail.com>
2022-08-25 14:28:45 +0100

    Store a list with the latest calculations. And give the users the ability to delete that list. (List Method)

--- Calculator/Calculate.cs
+++ Calculator/Calculate.cs
@@ -6,29 +6,31 @@ using System.Threading.Tasks;
 
 public class Calculate
 {
+    private static int id;
+    public static List<string> lastCalculation = new List<string>();
     public static double Input1 { get; set; }
     public static double Input2 { get; set; }
 
     public void DoAddition()
     {
         var ans = Input1 + Input2;
-
-        Console.WriteLine($"\n{Input1} + {Input2} = {ans}");
+        Console.WriteLine("");
+        Console.WriteLine(SaveFormat($"{Input1} + {Input2} = {ans}"));
     }
     public void DoSubstraction()
     {
 
         var ans = Input1 - Input2;
-
-        Console.WriteLine($"\n{Input1} - {Input2} = {ans}");
+        Console.WriteLine("");
+        Console.WriteLine(SaveFormat($"{Input1} - {Input2} = {ans}"));
     }
 
     public void DoMultiplication()
     {
 
         var ans = Input1 * Input2;
-
-        Console.WriteLine($"\n{Input1} * {Input2} = {ans}");
+        Console.WriteLine("");
+        Console.WriteLine(SaveFormat($"{Input1} * {Input2} = {ans}"));
     }
 
     public void DoDivision()
@@ -37,10 +39,14 @@ public class Calculate
 
         if (Input1 > 0 && Input2 == 0)
         {
-            Console.WriteLine($" {Input1} Cannot be divided by {Input2}");
+            Console.WriteLine("");
+            Console.WriteLine(SaveFormat($" {Input1} Cannot be divided by {Input2}"));
         }
         else
-            Console.WriteLine($"\n{Input1} / {Input2} = {ans}");
+        {
+            Console.WriteLine("");
+            Console.WriteLine(SaveFormat($"{Input1} / {Input2} = {ans}"));
+        }
     }
 
     public void DoDivisionAndReminider()
@@ -49,9 +55,21 @@ public class Calculate
         var rem = Input1 % Input1;
         if (Input1 > 0 && Input2 == 0)
         {
-            Console.WriteLine($" {Input1} Cannot be divided by {Input2}");
+            Console.WriteLine("");
+            Console.WriteLine(SaveFormat($" {Input1} Cannot be divided by {Input2}"));
         }
         else
-            Console.WriteLine($"\n{Input1} / {Input1} = {ans} remenider {rem}");
+        {
+            Console.WriteLine("");
+            Console.WriteLine(SaveFormat($"{Input1} / {Input1} = {ans} remenider {rem}"));
+        }
+    }
+
+    public string SaveFormat(string result)
+    {
+        lastCalculation.Add($"{id}) {result}");
+        id++;
+
+        return result;
     }
 }
